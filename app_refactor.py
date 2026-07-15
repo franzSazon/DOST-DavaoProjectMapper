e5690daf55bfd8ce315eacbcb1ec21efa43a79f7
Franz Sazon <jsazon067@gmail.com>
2026-07-15 11:19:04 +0800

    final styling changes

--- app_refactor.py
+++ app_refactor.py
@@ -1313,6 +1313,14 @@ st.markdown("""
             background-color: #FFFFFF !important;
         }
 
+        .st-emotion-cache-yiekhv {
+            background-color: #FFFFFF !important;
+        }
+
+        .st-g9 {
+            background-color: #FFFFFF !important;
+        }
+
         /* ==========================================
            13. SLIDER TWEAKS
            ========================================== */
